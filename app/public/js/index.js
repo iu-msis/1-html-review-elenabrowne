c2105de496e5d68febc14575fa5f9d4d69b6c1da
elenabrowne <elenbrow@iu.edu>
2021-09-20 12:06:50 -0400

    Index changes

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -11,14 +11,7 @@ const Offer = {
                     "company":"EY",
                     "offerDate": "2021-09-08"
                 },
-                {
-                    "id": 2,
-                    "name": "Jordan Doe",
-                    "salary": 80000,
-                    "bonus": 2000,
-                    "company":"IU",
-                    "offerDate": "2021-08-09"
-                }
+                
             ]
         }
     },
