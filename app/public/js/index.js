829d8c8e67ecd5cf093dc085271aec1c2cef7ef3
elenabrowne <elenbrow@iu.edu>
2021-10-20 13:52:06 -0400

    Update index.js

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,48 +1,147 @@
 const Offer = {
     data() {
       return {
-        "person": {},
-        "offers": [
-                {
-                    "id": 1,
-                    "name": "Jane Doe",
-                    "salary": 120000,
-                    "bonus": 9000,
-                    "company":"EY",
-                    "offerDate": "2021-09-08"
-                },
-                
-            ]
-        }
+        students: [],
+        selectedStudent: null,
+        offers: [],
+        offerForm: {},
+        selectedOffer: null
+      }
     },
-    computed: {
-        prettyBirthday(){
-            return dayjs(this.person.dob.date)
-            .format ('D MMM YYYY')
-        }
-    },
+    computed: {},
     methods: {
-      fetchUserData(){
-        console.log("A");
-        fetch('https://randomuser.me/api/')
-        .then( response => response.json() )
-        .then( (responseJson) => {
-            console.log(responseJson);
-            console.log("C");
-            this.person = responseJson.results[0];
-        })
-        .catch( (err) => {
-            console.error(err);
-        })
-        console.log("B");
-    } 
-}, 
+        prettyData(d) {
+            return dayjs(d)
+            .format('D MMM YYYY')
+        },
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
+        selectStudent(s) {
+            if (s == this.selectedStudent) {
+                return;
+            }
+            this.selectedStudent = s;
+            this.offers = [];
+            this.fetchOfferData(this.selectedStudent);
+        },
+        fetchStudentData() {
+            fetch('/api/student/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.students = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        fetchOfferData(s) {
+            console.log("Fetching offer data for ", s);
+            fetch('/api/offer/?student=' + s.id)
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.offers = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        },
+        postOffer(evt) {
+            console.log ("Test:", this.selectedOffer);
+          if (this.selectedOffer) {
+              this.postEditOffer(evt);
+          } else {
+              this.postNewOffer(evt);
+          }
+        },
+        postEditOffer(evt) {
+          this.offerForm.id = this.selectedOffer.id;
+          this.offerForm.studentId = this.selectedStudent.id;        
+          
+          console.log("Editing!", this.offerForm);
   
-
+          fetch('api/offer/update.php', {
+              method:'POST',
+              body: JSON.stringify(this.offerForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.offers = json;
+              
+              // reset the form
+              this.handleResetEdit();
+            });
+        },
+        postNewOffer(evt) {
+          this.offerForm.studentId = this.selectedStudent.id;        
+          
+          console.log("Creating!", this.offerForm);
+  
+          fetch('api/offer/create.php', {
+              method:'POST',
+              body: JSON.stringify(this.offerForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.offers = json;
+              
+              // reset the form
+              this.handleResetEdit();
+            });
+        },
+        postDeleteOffer(o) {  
+            if ( !confirm("Are you sure you want to delete the offer from " + o.companyName + "?") ) {
+                return;
+            }  
+            
+            console.log("Delete!", o);
+    
+            fetch('api/offer/delete.php', {
+                method:'POST',
+                body: JSON.stringify(o),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.offers = json;
+                
+                // reset the form
+                this.handleResetEdit();
+              });
+          },
+        handleEditOffer(offer) {
+            this.selectedOffer = offer;
+            this.offerForm = Object.assign({}, this.selectedOffer);
+        },
+        handleResetEdit() {
+            this.selectedOffer = null;
+            this.offerForm = {};
+        }
+    },
     created() {
-        this.fetchUserData();
-    } //end created
-} // end Offer config
+        this.fetchStudentData();
+    }
   
-Vue.createApp(Offer).mount('#offerApp');
-console.log("Z");
+  }
+  
+  Vue.createApp(Offer).mount('#offerApp');
